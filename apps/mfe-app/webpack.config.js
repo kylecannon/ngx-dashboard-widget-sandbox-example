7f2d160181b0ace1ea2e0c5baf15496bb29ae6b0
Kyle Cannon <kyle.d.cannon@gmail.com>
2021-01-24 13:19:18 -0800

    chore: generate mfe-app

---
+++ apps/mfe-app/webpack.config.js
@@ -0,0 +1,43 @@
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const mf = require('@angular-architects/module-federation/webpack');
+const path = require('path');
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(path.join(__dirname, '../../tsconfig.base.json'), [
+  /* mapped paths to share */
+]);
+
+module.exports = {
+  output: {
+    uniqueName: 'mfe-app',
+  },
+  optimization: {
+    // Only needed to bypass a temporary bug
+    runtimeChunk: false,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      // For remotes (please adjust)
+      // name: "mfe-app",
+      // filename: "remoteEntry.js",
+      // exposes: {
+      //     './Component': './apps/mfe-app/src/app/app.component.ts',
+      // },
+
+      // For hosts (please adjust)
+      // remotes: {
+      //     "ngx-dashboard-widget-sandbox-example": "ngx-dashboard-widget-sandbox-example@http://localhost:4200/remoteEntry.js",
+
+      // },
+
+      shared: {
+        '@angular/core': { singleton: true, strictVersion: true },
+        '@angular/common': { singleton: true, strictVersion: true },
+        '@angular/router': { singleton: true, strictVersion: true },
+
+        ...sharedMappings.getDescriptors(),
+      },
+    }),
+    sharedMappings.getPlugin(),
+  ],
+};
