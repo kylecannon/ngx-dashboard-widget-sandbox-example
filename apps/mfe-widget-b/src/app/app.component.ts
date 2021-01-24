9d4cca20af5e7ea2f7c9adea4cbe3e39fa828d95
Kyle Cannon <kyle.d.cannon@gmail.com>
2021-01-24 13:20:56 -0800

    chore: generate mfe-widget-a and mfe-widget-b

---
+++ apps/mfe-widget-b/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-dashboard-widget-sandbox-example-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'mfe-widget-b';
+}
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/mfe-app'],
+  projects: [
+    '<rootDir>/apps/mfe-app',
+    '<rootDir>/apps/mfe-widget-a',
+    '<rootDir>/apps/mfe-widget-b',
+  ],
 };
